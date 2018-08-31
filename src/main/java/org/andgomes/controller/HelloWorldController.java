31262c69987727b284919de4d3437920f19f72b2
Igor Machado <paulo.souza@anovasistemas.com.br>
2018-08-31 12:24:49 -0300

    springao2 funfando

---
+++ src/main/java/org/andgomes/controller/HelloWorldController.java
@@ -0,0 +1,37 @@
+package org.andgomes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping({"/hello"})
+public class HelloWorldController {
+
+	String message = "welcome, user";
+	
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(@RequestParam(value = "name", required = false, defaultValue = "World") String name){
+		
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		
+		return mv;
+	}
+	
+	
+	@RequestMapping("/oi")
+	public String sayHello() {
+		System.out.println("oi.");
+		return "oi";
+	}
+
+	@RequestMapping("/tchau")
+	public String sayGoodBye() {
+		System.out.println("tchau.");
+		return "bye";
+	}
+
+}
